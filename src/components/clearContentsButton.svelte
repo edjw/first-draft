b7c968a26e72dfe20435b070ac02573938040d22
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2020-09-09 12:24:38 +0100

    add a start again button

---
+++ src/components/clearContentsButton.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { contents } from "../components/stores.js";
+
+  const clearContents = () => {
+    // const startingText = `{"ops":[{"insert":"\n"}]}`;
+    $contents.html = undefined;
+    $contents.contents = undefined;
+    $contents.plainText = undefined;
+    document.querySelector(".ql-editor").innerHTML = "";
+  };
+</script>
+
+<button on:click={clearContents}>Start again</button>
